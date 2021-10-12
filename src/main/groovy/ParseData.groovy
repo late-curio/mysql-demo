6cf4c4997fabab7e2a220ec5d4b44bb8b688b424
Todd W Crone <twcrone@gmail.com>
2021-10-11 23:48:42 -0400

    Run against all test directories in base directory

--- src/main/groovy/ParseData.groovy
+++ src/main/groovy/ParseData.groovy
@@ -1,73 +1,81 @@
+import groovy.io.FileType
+
 import java.time.Duration
 
-def run = args[0]
-def dir = "/Users/tcrone/temp/mysql/${run}"
-println "Loading data from $dir"
-println "```"
-println run
 
-BigInteger count = 0
-BigInteger sum = 0
-String mysqlVersion = null
-String agentVersion = null
-BigInteger start = -1
-BigInteger end = 0;
-new File("$dir/report.csv").splitEachLine(",") { fields ->
-    if(fields == null || fields.size() < 4) {
-        println "Bad data $fields"
-    }
-    else {
-        def s = fields[1]
-        if(s.isNumber()) {
-            def ts = new BigInteger(fields[0])
-            if(start == -1) {
-                start = ts
-            }
-            else {
-                end = ts
-            }
-            def ms = new BigInteger(s)
-            count = count.add(BigInteger.ONE)
-            sum = sum.add(ms)
-            if(count == 1) {
-                mysqlVersion = fields[2]
-                agentVersion = fields[3]
-            }
-//            if(ms > 999) {
-//                println ms
-//            }
-        }
+File baseDir = new File("/Users/tcrone/temp/mysql")
+println "Loading data from ${baseDir.getName()}"
 
-    }
-}
-def average = sum.divide(count)
-def elapsedMs = end.subtract(start)
-def duration = Duration.ofMillis(elapsedMs.toLong())
-
-BigInteger sampleCount = 0
-BigInteger memsum = 0
-
-new File("$dir/system.csv").splitEachLine(",") { fields ->
-    if(fields == null || fields.size() < 6) {
-        println "Bad data $fields"
-    }
-    else {
-        def s = fields[3]
-        if(s.isNumber()) {
-            def ms = new BigInteger(s)
-            sampleCount = sampleCount.add(BigInteger.ONE)
-            memsum = memsum.add(ms)
-        }
-
-    }
+baseDir.eachFile(FileType.DIRECTORIES) { file ->
+    doEet(baseDir, file.getName())
 }
 
-averageMemory = memsum.divide(sampleCount)
+def doEet(baseDir, run) {
+    def dir = "/Users/tcrone/temp/mysql/${run}"
+    println "```"
+    println run
 
-println "MySQL version: $mysqlVersion"
-println "Agent version: $agentVersion"
-println "Count of rows: $count"
-println "Elapsed time: $duration"
-println "Average time is: ${average}ms"
-println "Average memory used is: $averageMemory"
-println "```"
+    BigInteger count = 0
+    BigInteger sum = 0
+    String mysqlVersion = null
+    String agentVersion = null
+    BigInteger start = -1
+    BigInteger end = 0;
+    new File("$dir/report.csv").splitEachLine(",") { fields ->
+        if(fields == null || fields.size() < 4) {
+            println "Bad data $fields"
+        }
+        else {
+            def s = fields[1]
+            if(s.isNumber()) {
+                def ts = new BigInteger(fields[0])
+                if(start == -1) {
+                    start = ts
+                }
+                else {
+                    end = ts
+                }
+                def ms = new BigInteger(s)
+                count = count.add(BigInteger.ONE)
+                sum = sum.add(ms)
+                if(count == 1) {
+                    mysqlVersion = fields[2]
+                    agentVersion = fields[3]
+                }
+            }
+
+        }
+    }
+    def average = sum.divide(count)
+    def elapsedMs = end.subtract(start)
+    def duration = Duration.ofMillis(elapsedMs.toLong())
+
+    BigInteger sampleCount = 0
+    BigInteger memsum = 0
+
+    new File("$dir/system.csv").splitEachLine(",") { fields ->
+        if(fields == null || fields.size() < 6) {
+            println "Bad data $fields"
+        }
+        else {
+            def s = fields[3]
+            if(s.isNumber()) {
+                def ms = new BigInteger(s)
+                sampleCount = sampleCount.add(BigInteger.ONE)
+                memsum = memsum.add(ms)
+            }
+
+        }
+    }
+
+    averageMemory = memsum.divide(sampleCount)
+
+    println "MySQL version: $mysqlVersion"
+    println "Agent version: $agentVersion"
+    println "Count of rows: $count"
+    println "Elapsed time: $duration"
+    println "Average time is: ${average}ms"
+    println "Average memory used is: $averageMemory"
+    println "```"
+
+}
