1804668331f2ee86c89574ab1a7be193db04e3af
Todd W Crone <twcrone@gmail.com>
2021-12-20 16:48:46 -0500

    Cleanup 'runWithAgent.sh'

--- runMySQL.sh
+++ runMySQL.sh
@@ -1,1 +1,1 @@
-docker run --name test-mysql-$1 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=spring_app_db -d mysql:$1
+docker run --name test-mysql8 -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=spring_app_db -d mysql:8
--- runWithAgent.sh
+++ runWithAgent.sh
@@ -1,7 +1,5 @@
-#agent="/opt/newrelic/newrelic.jar"
-agent=/Users/tcrone/lib/nrjars/newrelic-agent-7.3.0.jar
+agent="/opt/newrelic/newrelic.jar"
 config="/opt/newrelic/newrelic.yml"
-#config=/Users/tcrone/conf/disable-mysql.yml
 env="staging"
 jar="build/libs/mysql-demo-0.0.1-SNAPSHOT.jar"
 ./gradlew build
