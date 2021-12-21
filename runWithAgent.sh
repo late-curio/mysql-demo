a025c0bc33c7c7beb778c03cdb003eab186e914d
Todd W Crone <twcrone@gmail.com>
2021-12-21 16:44:41 -0500

    Make bootRun and runWithAgent.sh configurable

--- runWithAgent.sh
+++ runWithAgent.sh
@@ -1,6 +1,3 @@
-agent="/opt/newrelic/newrelic.jar"
-config="/opt/newrelic/newrelic.yml"
-env="staging"
 jar="build/libs/mysql-demo-0.0.1-SNAPSHOT.jar"
 ./gradlew build
-java -Dnewrelic.config.file=$config -Dnewrelic.environment=$env -javaagent:$agent -jar $jar
+java -Dnewrelic.config.file=$NRCONFIG -Dnewrelic.environment=$NRENV -javaagent:$NRAGENT -jar $jar
